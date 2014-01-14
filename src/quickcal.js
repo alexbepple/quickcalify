489afda8dae2fc2e85153936cee3fb21160407bd
Alex Bepple <alex@bepple.de>
2014-01-14 12:21:40 +0100

    + tod → today, tom → tomorrow

--- src/quickcal.js
+++ src/quickcal.js
@@ -1,6 +1,16 @@
 
+var abbreviations = {
+    'tod': 'today',
+    'tom': 'tomorrow'
+};
+
+var expandAbbreviations = function (match) {
+    if (abbreviations.hasOwnProperty(match)) return abbreviations[match];
+    return match;
+};
+
 var translate = function (input) {
-    return input;
+    return input.replace(/\w+/, expandAbbreviations);
 };
 
 exports.translate = translate;
--- test/unit/QuickCal_Spec.js
+++ test/unit/QuickCal_Spec.js
@@ -5,4 +5,13 @@ describe('QuickCalifier', function() {
     it('returns input unchanged when there is nothing to do', function() {
         expect(translate('foo')).to.equal('foo');
     });
+
+    describe('expands abbreviations', function() {
+        it('today', function() {
+            expect(translate('tod')).to.contain('today');
+        });
+        it('tomorrow', function() {
+            expect(translate('tom')).to.contain('tomorrow');
+        });
+    });
 });
